b471dc8fb1738f46b513d36bae2a63fec20ebab0
akj1 <kellerr@abv.bg>
2015-06-01 18:47:48 +0300

    Added MVC Expressions Library

--- OnixiaWebGame/Onixia.Models/SpaceObject.cs
+++ OnixiaWebGame/Onixia.Models/SpaceObject.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Onixia.Models
+﻿namespace Onixia.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
     public class SpaceObject
     {
-
         #region Properties
         [Required]
         public float PosX { get; set; }
