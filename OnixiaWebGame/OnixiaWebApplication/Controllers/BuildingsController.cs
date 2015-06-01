711febc9023d6cabde302583ca3a5f57eb78b3cf
Vassil Nikov <vassiln@yahoo.com>
2015-06-02 00:49:27 +0300

    added images, game css file and leftnavbar menu

---
+++ OnixiaWebGame/OnixiaWebApplication/Controllers/BuildingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnixiaWebApplication.Controllers
+{
+    public class BuildingsController : Controller
+    {
+        // GET: Buildings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
