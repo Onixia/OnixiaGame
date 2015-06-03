378a5be5b57352a04b477d08cfa5b8dd3d31c47f
BAleksiev <b.aleksiev@gmail.com>
2015-06-03 15:47:46 +0300

    refresh resources

--- OnixiaWebGame/OnixiaWebApplication/Controllers/BaseController.cs
+++ OnixiaWebGame/OnixiaWebApplication/Controllers/BaseController.cs
@@ -3,11 +3,13 @@
     using System;
     using System.Web.Mvc;
     using System.Web.Routing;
+    using System.Linq;
 
     using Microsoft.AspNet.Identity;
 
     using Onixia.Data.Contracts;
     using Onixia.Models;
+    using Onixia.Models.ObjectTemplates;
 
     public abstract class BaseController : Controller
     {
@@ -20,6 +22,7 @@
         protected BaseController(IOnixiaData data)
         {
             this.Data = data;
+            refreshResources();
         }
 
         protected User UserProfile { get; private set; }
@@ -37,5 +40,35 @@
         }
 
         protected IOnixiaData Data { get; set; }
+
+        protected void refreshResources()
+        {
+            if (this.UserProfile != null)
+            {
+                var buildings = this.Data.Buildings.All();
+                var userPlanet = this.UserProfile.Planets.FirstOrDefault();
+
+                var currentTime = DateTime.Now;
+
+                //if (userPlanet.LastUpdatedOn <= currentTime.AddMinutes(-1))
+                //{
+                var metalMineLevel = userPlanet.Buildings.FirstOrDefault(b => b.Building.BuildingType == BuildingType.Metal).BuildingLevel;
+                var crystalMineLevel = userPlanet.Buildings.FirstOrDefault(b => b.Building.BuildingType == BuildingType.Crystal).BuildingLevel;
+                var gasMineLevel = userPlanet.Buildings.FirstOrDefault(b => b.Building.BuildingType == BuildingType.Gas).BuildingLevel;
+                var solarPanelsLevel = userPlanet.Buildings.FirstOrDefault(b => b.Building.BuildingType == BuildingType.SolarPanels).BuildingLevel;
+
+                var metalIncome = metalMineLevel * buildings.FirstOrDefault(b => b.BuildingType == BuildingType.Metal).Income.Metal;
+                var crystalIncome = metalMineLevel * buildings.FirstOrDefault(b => b.BuildingType == BuildingType.Metal).Income.Metal;
+                var gasIncome = metalMineLevel * buildings.FirstOrDefault(b => b.BuildingType == BuildingType.Metal).Income.Metal;
+                var energyIncome = metalMineLevel * buildings.FirstOrDefault(b => b.BuildingType == BuildingType.Metal).Income.Metal;
+
+                userPlanet.PlanetResourceses.Metal += metalIncome;
+                userPlanet.PlanetResourceses.Crystal += crystalIncome;
+                userPlanet.PlanetResourceses.Gas += gasIncome;
+                userPlanet.PlanetResourceses.Energy += energyIncome;
+                this.Data.SaveChanges();
+                //}
+            }
+        }
     }
 }
